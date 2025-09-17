536dee4666ffc59f610bb30d69586d5e8af78447
Aruzhan <zhumabaiaruzhan0@gmail.com>
2025-09-17 22:15:02 +0500

    .py
    "added lab1 files"

---
+++ lab1/glob.py
@@ -0,0 +1,34 @@
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+#To change the value of a global variable inside a function, refer to the variable by using the global keyword:
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
